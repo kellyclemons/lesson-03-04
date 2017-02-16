ee93e73a51a8bfe5ecadfe1d24c1707cff798727
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-16 10:52:13 -0600

    Made a simple element and appended it

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 import 'whatwg-fetch';
+
+const grid = document.querySelector('.grid');
+
+// Create a new div element in memory
+const gridItem = document.createElement('div');
+
+// NOTE: We are adding a class name not specifying a class query selector
+// So no `.` here
+gridItem.classList.add('grid__item');
+gridItem.innerText = 'YAY I MADE A NEW ELEMENT!';
+console.log(gridItem);
+
+// Add the gridItem to the end of the "grid" element
+grid.appendChild(gridItem);
