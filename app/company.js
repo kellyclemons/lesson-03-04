d56db9c1b3e9a75d36dfbcef9b3e87c81891a34a
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-16 10:30:10 -0600

    feat: data and mock HTML & CSS

---
+++ app/company.js
@@ -0,0 +1,121 @@
+export default [{
+  "city": "Cergy-Pontoise",
+  "skill": "DWDM",
+  "company": "Yakitri",
+  "country": "France",
+  "phone": "33-(921)796-0086"
+}, {
+  "city": "Montreuil",
+  "skill": "Juvenile Justice",
+  "company": "Topicshots",
+  "country": "France",
+  "phone": "33-(690)300-2725"
+}, {
+  "city": "Tuscaloosa",
+  "skill": "Equipment Repair",
+  "company": "Dynava",
+  "country": "United States",
+  "phone": "1-(205)885-6879"
+}, {
+  "city": "Hénin-Beaumont",
+  "skill": "Quality Improvement",
+  "company": "Nlounge",
+  "country": "France",
+  "phone": "33-(883)160-9268"
+}, {
+  "city": "Aldergrove",
+  "skill": "Semantic HTML",
+  "company": "Lazzy",
+  "country": "Canada",
+  "phone": "1-(822)375-4357"
+}, {
+  "city": "Lincoln",
+  "skill": "PQRI",
+  "company": "Vitz",
+  "country": "United States",
+  "phone": "1-(402)124-4486"
+}, {
+  "city": "Newport",
+  "skill": "EEO/AA Compliance",
+  "company": "Twimbo",
+  "country": "United Kingdom",
+  "phone": "44-(254)941-4791"
+}, {
+  "city": "Labelle",
+  "skill": "Multiplex PCR",
+  "company": "Tagcat",
+  "country": "Canada",
+  "phone": "1-(232)468-6448"
+}, {
+  "city": "Schiltigheim",
+  "skill": "MBE",
+  "company": "Roomm",
+  "country": "France",
+  "phone": "33-(819)422-9490"
+}, {
+  "city": "Pincher Creek",
+  "skill": "MPLS",
+  "company": "Feedfish",
+  "country": "Canada",
+  "phone": "1-(241)157-4738"
+}, {
+  "city": "Bastia",
+  "skill": "Organizational Leadership",
+  "company": "Edgeify",
+  "country": "France",
+  "phone": "33-(378)612-5761"
+}, {
+  "city": "Paris 12",
+  "skill": "Diabetes",
+  "company": "Jaxspan",
+  "country": "France",
+  "phone": "33-(620)477-5024"
+}, {
+  "city": "Mulhouse",
+  "skill": "Subcontracting",
+  "company": "Riffpath",
+  "country": "France",
+  "phone": "33-(118)392-7536"
+}, {
+  "city": "Raleigh",
+  "skill": "PMAs",
+  "company": "Jabbersphere",
+  "country": "United States",
+  "phone": "1-(919)177-7083"
+}, {
+  "city": "Loudun",
+  "skill": "CNG",
+  "company": "Wordify",
+  "country": "France",
+  "phone": "33-(981)617-1300"
+}, {
+  "city": "Newton",
+  "skill": "Funding",
+  "company": "Lajo",
+  "country": "United Kingdom",
+  "phone": "44-(756)426-0874"
+}, {
+  "city": "Detroit",
+  "skill": "ebXML",
+  "company": "Edgetag",
+  "country": "United States",
+  "phone": "1-(313)551-4181"
+}, {
+  "city": "Saint-Flour",
+  "skill": "Wireframes",
+  "company": "Wordtune",
+  "country": "France",
+  "phone": "33-(518)330-0462"
+}, {
+  "city": "Windsor",
+  "skill": "IOS Firewall",
+  "company": "Realmix",
+  "country": "Canada",
+  "phone": "1-(426)280-6896"
+}, {
+  "city": "Cergy-Pontoise",
+  "skill": "Cardiology",
+  "company": "Bluejam",
+  "country": "France",
+  "phone": "33-(670)397-7077"
+}];
