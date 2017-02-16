15fb4917bf4c2ec9ae365d8aaa3f9dd0960e4496
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-16 11:57:31 -0600

    Basic example of our function to create an element

--- app/index.js
+++ app/index.js
@@ -2,23 +2,36 @@ import 'whatwg-fetch';
 
 const grid = document.querySelector('.grid');
 
-// Create a new div element in memory
-const gridItem = document.createElement('div');
+/**
+ * Creates a grid__item element filled in with a profile information
+ * @param  {object} profile
+ * @return {Element}
+ */
+function createProfileElement(profile) {
+  // Create a new div element in memory
+  const gridItem = document.createElement('div');
 
-// NOTE: We are adding a class name not specifying a class query selector
-// So no `.` here
-gridItem.classList.add('grid__item');
-gridItem.innerHTML = `
-<div class="profile">
-  <h2 class="profile__company">Company Name</h2>
+  // NOTE: We are adding a class name not specifying a class query selector
+  // So no `.` here
+  gridItem.classList.add('grid__item');
+  gridItem.innerHTML = `
+  <div class="profile">
+    <h2 class="profile__company">Company Name</h2>
 
-  <h3 class="profile__location">Country, City</h3>
+    <h3 class="profile__location">Country, City</h3>
 
-  <p class="profile__skill">Skill</p>
-</div>`;
+    <p class="profile__skill">Skill</p>
+  </div>`;
 
-gridItem.querySelector('.profile__company').innerText = 'Yahoo';
+  gridItem.querySelector('.profile__company').innerText = profile.company;
+  gridItem.querySelector('.profile__location').innerText = `${profile.country}, ${profile.city}`;
+  gridItem.querySelector('.profile__skill').innerText = profile.skill;
 
-// Add the gridItem to the end of the "grid" element
-grid.appendChild(gridItem);
+  return gridItem;
+}
+
+const yahoo = createProfileElement({ company: 'Yahoo' });
+
+// Add the gridItem for Yahoo to the end of the "grid" element
+grid.appendChild(yahoo);
 // document.body.querySelector('.profile__company').innerText = 'THIS IS THE FIRST ONE!!!';
